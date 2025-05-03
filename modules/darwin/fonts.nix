9478336aa5b954877cfa6dd9a360d9eac2621d98
Luca Maraschi <luca.maraschi@gmail.com>
2025-05-02 22:41:28 -0700

    updated install script

--- modules/darwin/fonts.nix
+++ modules/darwin/fonts.nix
@@ -6,8 +6,10 @@
     
     # Install these fonts
     packages = with pkgs; [
+      
       nerd-fonts.fira-code
       nerd-fonts.jetbrains-mono
+
       font-awesome
       material-design-icons
       tenderness
