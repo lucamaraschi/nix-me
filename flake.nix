d255c94e3acdb5a19822162d75440f4df1c7d9fc
Luca Maraschi <luca.maraschi@gmail.com>
2025-07-19 20:08:54 -0700

    Add binary substituters for NixOS VM

--- flake.nix
+++ flake.nix
@@ -158,8 +158,22 @@
       nixosConfigurations = {
         nixos-vm = nixpkgs.lib.nixosSystem {
           system = "aarch64-linux";
-          specialArgs = { inherit inputs; };
+          specialArgs = { inherit inputs username; };
           modules = [
+            # Add this module to configure substituters
+            {
+              nix.settings = {
+                substituters = [
+                  "https://cache.nixos.org/"
+                  "https://nix-community.cachix.org"
+                ];
+                trusted-public-keys = [
+                  "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+                  "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+                ];
+              };
+            }
+            
             ./hosts/nixos-vm/default.nix
             home-manager.nixosModules.home-manager
             {
@@ -167,7 +181,7 @@
                 useGlobalPkgs = true;
                 useUserPackages = true;
                 users.dev = import ./hosts/nixos-vm/home.nix;
-                extraSpecialArgs = { inherit inputs; };
+                extraSpecialArgs = { inherit inputs username; };
               };
             }
           ];
