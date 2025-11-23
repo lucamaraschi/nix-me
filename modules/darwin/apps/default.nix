e37773588f178284c3e93278c5167f29fa7a7cc7
Luca Maraschi <luca.maraschi@gmail.com>
2025-11-22 16:13:32 -0800

    Fix: Remove import of empty raycast.nix file
    The raycast.nix file is empty and was causing a syntax error during build:
    "syntax error, unexpected end of file at raycast.nix:1:1"
    
    Removed from imports until we add actual Raycast configuration.
    Marked as comment for future use.

--- modules/darwin/apps/default.nix
+++ modules/darwin/apps/default.nix
@@ -4,12 +4,12 @@
   imports = [
     ./installations.nix
     ./nix-me.nix
-    ./raycast.nix
     ./rectangle.nix
     ./vm-manager.nix
     # Add more system-level app configurations here:
     # ./hammerspoon.nix
     # ./hiddenbar.nix
+    # ./raycast.nix (empty file - add config when needed)
     # ./docker.nix
   ];
 }
