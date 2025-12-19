d1ddc85f5461b394e699850c4bb6300c81dee469
Luca Maraschi <luca.maraschi@gmail.com>
2025-12-18 19:54:58 -0800

    Fix deprecation warnings for git and ssh home-manager options

--- modules/home-manager/apps/git.nix
+++ modules/home-manager/apps/git.nix
@@ -3,33 +3,36 @@
 {
   programs.git = {
     enable = true;
-    userName = "Luca Maraschi";
-    userEmail = "luca.maraschi@gmail.com";
     lfs.enable = true;
-    
-    extraConfig = {
-      init.defaultBranch = "main";
-      pull.rebase = false;
-      core.editor = "vim";
-      push.autoSetupRemote = true;
-    };
-    
-    aliases = {
-      co = "checkout";
-      ci = "commit";
-      st = "status";
-      br = "branch";
-      amend = "commit --amend";
-      unstage = "reset HEAD --";
-      last = "log -1 HEAD";
-      sync-branches = "!git fetch --prune && git branch -vv | grep ': gone]' | awk '{print $1}' | xargs git branch -D";
-    };
-    
+
     ignores = [
       ".DS_Store"
       "*.swp"
       ".direnv"
       "node_modules"
     ];
+
+    settings = {
+      user = {
+        name = "Luca Maraschi";
+        email = "luca.maraschi@gmail.com";
+      };
+
+      init.defaultBranch = "main";
+      pull.rebase = false;
+      core.editor = "vim";
+      push.autoSetupRemote = true;
+
+      alias = {
+        co = "checkout";
+        ci = "commit";
+        st = "status";
+        br = "branch";
+        amend = "commit --amend";
+        unstage = "reset HEAD --";
+        last = "log -1 HEAD";
+        sync-branches = "!git fetch --prune && git branch -vv | grep ': gone]' | awk '{print $1}' | xargs git branch -D";
+      };
+    };
   };
 }
--- modules/home-manager/apps/ssh.nix
+++ modules/home-manager/apps/ssh.nix
@@ -3,6 +3,7 @@
 {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;  # We set our own defaults in matchBlocks."*"
 
     matchBlocks = {
       "github.com" = {
