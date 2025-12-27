a839e3d2caad6ac5e334260948d0d7f3956ed4f5
Luca Maraschi <luca.maraschi@gmail.com>
2025-12-27 12:17:36 -0800

    Fix Rectangle: use macOS defaults and add launchd agent for auto-start

--- modules/home-manager/rectangle.nix
+++ modules/home-manager/rectangle.nix
@@ -1,80 +1,45 @@
 # modules/home-manager/rectangle.nix
+# Rectangle window manager configuration for macOS
+# Uses macOS defaults system (plist) instead of JSON config
 { config, lib, pkgs, ... }:
 
 {
-  # Rectangle window manager configuration
-  home.file.".config/rectangle/RectangleConfig.json".text = builtins.toJSON {
+  # Rectangle preferences via activation script
+  # Rectangle stores settings in ~/Library/Preferences/com.knollsoft.Rectangle.plist
+  home.activation.rectangleConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     # General settings
-    SUEnableAutomaticChecks = true;
-    launchOnLogin = true;
-    hideMenubarIcon = false;
-    alternateDefaultShortcuts = true;
-    allowAnyShortcut = true;
-    
-    # Window behavior
-    gapSize = 5;
-    snapEdgeMarginTop = 5;
-    snapEdgeMarginBottom = 5;
-    snapEdgeMarginLeft = 5;
-    snapEdgeMarginRight = 5;
-    
-    # Size settings
-    centerHalfCycles = [
-      "50"
-      "67"
-      "75"
-    ];
-    centeredDirectionalMove = false;
-    almostMaximizeHeight = 95;
-    almostMaximizeWidth = 95;
-    
-    # Appearance
-    windowSnapping = true;
-    todoMode = false;
-    
-    # Keyboard shortcuts (using Hyper key (Cmd+Ctrl+Alt+Shift) for most operations)
-    shortcuts = {
-      # Basic positioning
-      leftHalf = "^⌥⌘←";            # Ctrl+Option+Cmd+Left
-      rightHalf = "^⌥⌘→";           # Ctrl+Option+Cmd+Right
-      topHalf = "^⌥⌘↑";             # Ctrl+Option+Cmd+Up
-      bottomHalf = "^⌥⌘↓";          # Ctrl+Option+Cmd+Down
-      
-      # Corners
-      topLeft = "^⌥⌘U";             # Ctrl+Option+Cmd+U
-      topRight = "^⌥⌘I";            # Ctrl+Option+Cmd+I
-      bottomLeft = "^⌥⌘J";          # Ctrl+Option+Cmd+J
-      bottomRight = "^⌥⌘K";         # Ctrl+Option+Cmd+K
-      
-      # Thirds
-      leftThird = "^⌥⌘D";           # Ctrl+Option+Cmd+D
-      centerThird = "^⌥⌘F";         # Ctrl+Option+Cmd+F
-      rightThird = "^⌥⌘G";          # Ctrl+Option+Cmd+G
-      
-      # Full screen operations
-      maximize = "^⌥⌘M";            # Ctrl+Option+Cmd+M
-      maximizeHeight = "^⌥⌘H";      # Ctrl+Option+Cmd+H
-      
-      # Center operations
-      center = "^⌥⌘C";              # Ctrl+Option+Cmd+C
-      centerHalf = "^⌥⌘V";          # Ctrl+Option+Cmd+V
-      
-      # Screen movement
-      nextDisplay = "^⌥⌘]";         # Ctrl+Option+Cmd+]
-      previousDisplay = "^⌥⌘[";     # Ctrl+Option+Cmd+[
-      
-      # Size adjustments
-      restore = "^⌥⌘=";             # Ctrl+Option+Cmd+=
-      smaller = "^⌥⌘-";             # Ctrl+Option+Cmd+-
-      larger = "^⌥⌘+";              # Ctrl+Option+Cmd++
+    $DRY_RUN_CMD /usr/bin/defaults write com.knollsoft.Rectangle SUEnableAutomaticChecks -bool true
+    $DRY_RUN_CMD /usr/bin/defaults write com.knollsoft.Rectangle launchOnLogin -bool true
+    $DRY_RUN_CMD /usr/bin/defaults write com.knollsoft.Rectangle hideMenubarIcon -bool false
+    $DRY_RUN_CMD /usr/bin/defaults write com.knollsoft.Rectangle alternateDefaultShortcuts -bool true
+    $DRY_RUN_CMD /usr/bin/defaults write com.knollsoft.Rectangle allowAnyShortcut -bool true
+
+    # Window behavior - gap size
+    $DRY_RUN_CMD /usr/bin/defaults write com.knollsoft.Rectangle gapSize -float 5
+    $DRY_RUN_CMD /usr/bin/defaults write com.knollsoft.Rectangle snapEdgeMarginTop -int 5
+    $DRY_RUN_CMD /usr/bin/defaults write com.knollsoft.Rectangle snapEdgeMarginBottom -int 5
+    $DRY_RUN_CMD /usr/bin/defaults write com.knollsoft.Rectangle snapEdgeMarginLeft -int 5
+    $DRY_RUN_CMD /usr/bin/defaults write com.knollsoft.Rectangle snapEdgeMarginRight -int 5
+
+    # Window snapping
+    $DRY_RUN_CMD /usr/bin/defaults write com.knollsoft.Rectangle windowSnapping -int 2
+
+    # Almost maximize settings (percentage)
+    $DRY_RUN_CMD /usr/bin/defaults write com.knollsoft.Rectangle almostMaximizeHeight -float 0.95
+    $DRY_RUN_CMD /usr/bin/defaults write com.knollsoft.Rectangle almostMaximizeWidth -float 0.95
+
+    # Cycling behavior
+    $DRY_RUN_CMD /usr/bin/defaults write com.knollsoft.Rectangle subsequentExecutionMode -int 1
+  '';
+
+  # Add Rectangle to Login Items via launchd
+  launchd.agents.rectangle = {
+    enable = true;
+    config = {
+      Label = "com.knollsoft.Rectangle";
+      ProgramArguments = [ "/Applications/Rectangle.app/Contents/MacOS/Rectangle" ];
+      RunAtLoad = true;
+      KeepAlive = false;
     };
-    
-    # List of apps to ignore
-    appsIgnored = [
-      "com.apple.ActivityMonitor"
-      "com.apple.DigitalColorMeter"
-      "com.apple.systempreferences"
-      "com.apple.finder"
-    ];
   };
 }
