50889c034b559cf1f53c60317786758f18b5ea18
Luca Maraschi <luca.maraschi@gmail.com>
2026-03-13 07:40:53 -0700

    Skip nodejs_22 tests - network tests fail in nix sandbox

--- overlays/nodejs.nix
+++ overlays/nodejs.nix
@@ -1,11 +1,8 @@
 final: prev:
 
 {
-  nodejs-22_14 = prev.nodejs.overrideAttrs (oldAttrs: rec {
-    version = "22.14.0";
-    src = prev.fetchurl {
-      url = "https://nodejs.org/download/release/v${version}/node-v${version}.tar.xz";
-      hash = "sha256-pzD3xdOc2o0V1vZ3iJ3zdC7uJKp2qHQNv0Bi9qp+NKY=";
-    };
+  # Skip tests for nodejs_22 - network tests fail in nix sandbox
+  nodejs_22 = prev.nodejs_22.overrideAttrs (oldAttrs: {
+    doCheck = false;
   });
 }
